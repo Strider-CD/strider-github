5cd9909dc601aa76c8ff08418f86656a1ccd3ad0
freewil <sean@eternalrise.com>
2014-03-01 22:47:28 -0800

    fix `createHooks()` logic for non-201 responses

--- test/test_api.js
+++ test/test_api.js
@@ -20,4 +20,26 @@ describe('github api', function () {
       })
     })
   })
+  
+  describe('createHooks', function () {
+    it('should fail on bad credentials', function (done) {
+      api.createHooks('github/github-services', 'http://example.com/hook', 'testsecret', 'invalidtoken', function (err) {
+        expect(err).to.be.a(Error)
+        done()
+      })
+    })
+    
+    // if test environment hasn't been set-up with test values then
+    // just make mocha report them as pending, rather than fail
+    var env = process.env
+      , t = env.TEST_HOOK_REPONAME ? 'it' : 'xit'
+      
+    global[t]('should create a hook', function (done) {
+      api.createHooks(env.TEST_HOOK_REPONAME, env.TEST_HOOK_URL, 'testsecret123', env.TEST_HOOK_TOKEN, function (err) {
+        expect(err).to.equal(null)
+        done()
+      })
+    })
+  })
+  
 })
